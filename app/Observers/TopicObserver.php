6a28089e474a013848f6e251b321d4a0924fd3cf
cancan <1401740794@qq.com>
2018-04-13 10:40:19 +0000

    User can delete reply

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Jobs\TranslateSlug;
+use Illuminate\Support\Facades\DB;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -32,5 +33,10 @@ class TopicObserver
     public function updating(Topic $topic)
     {
         //
+    }
+
+    public function deleted(Topic $topic)
+    {
+        DB::table('replies')->where('topic_id', $topic->id)->delete();
     }
 }
